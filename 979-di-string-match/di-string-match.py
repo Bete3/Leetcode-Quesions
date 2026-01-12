dfce32d57a7cc507b255c0b2a5931ceee27e6693
Betelhem Yehuala <167322606+Bete3@users.noreply.github.com>
2026-01-13 00:51:10 +0300

    Time: 0 ms (100.00%) | Memory: 20 MB (8.00%) - LeetSync

---
+++ 979-di-string-match/di-string-match.py
@@ -0,0 +1,15 @@
+class Solution:
+    def diStringMatch(self, s: str) -> List[int]:
+        low, high = 0, len(s)
+        result = []
+        
+        for c in s:
+            if c == 'I':
+                result.append(low)
+                low += 1
+            else:
+                result.append(high)
+                high -= 1
+        
+        result.append(low)  
+        return result
