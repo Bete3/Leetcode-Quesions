77426e4cf7519ff335809c6d18899aa7be6a55f8
Betelhem Yehuala <167322606+Bete3@users.noreply.github.com>
2026-01-13 20:36:55 +0300

    Time: 2439 ms (24.84%) | Memory: 19.5 MB (8.85%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.py
@@ -0,0 +1,39 @@
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+        empty = []
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] != '.':
+                    val = board[r][c]
+                    rows[r].add(val)
+                    cols[c].add(val)
+                    boxes[(r//3)*3 + c//3].add(val)
+                else:
+                    empty.append((r, c))
+
+        def backtrack(idx=0):
+            if idx == len(empty):
+                return True
+            r, c = empty[idx]
+            box_idx = (r//3)*3 + c//3
+            for val in map(str, range(1, 10)):
+                if val not in rows[r] and val not in cols[c] and val not in boxes[box_idx]:
+                    board[r][c] = val
+                    rows[r].add(val)
+                    cols[c].add(val)
+                    boxes[box_idx].add(val)
+                    if backtrack(idx + 1):
+                        return True
+                    board[r][c] = '.'
+                    rows[r].remove(val)
+                    cols[c].remove(val)
+                    boxes[box_idx].remove(val)
+            return False
+        backtrack()
