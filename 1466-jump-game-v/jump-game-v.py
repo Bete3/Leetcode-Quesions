0a38a528d0df5a15b07cbbf70860fb7d75aad866
Betelhem Yehuala <167322606+Bete3@users.noreply.github.com>
2026-01-26 21:13:16 +0300

    Time: 219 ms (71.70%) | Memory: 21.3 MB (35.85%) - LeetSync

---
+++ 1466-jump-game-v/jump-game-v.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxJumps(self, arr: List[int], d: int) -> int:
+        n = len(arr)
+        dp = [-1] * n
+
+        def dfs(i):
+            if dp[i] != -1:
+                return dp[i]
+            res = 1
+            for j in range(i + 1, min(i + d + 1, n)):
+                if arr[j] >= arr[i]:
+                    break
+                res = max(res, 1 + dfs(j))
+            for j in range(i - 1, max(i - d - 1, -1), -1):
+                if arr[j] >= arr[i]:
+                    break
+                res = max(res, 1 + dfs(j))
+            dp[i] = res
+            return res
+
+        return max(dfs(i) for i in range(n))
